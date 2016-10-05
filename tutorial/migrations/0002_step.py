a2dfc64d71892831b919f7101f2ed4e98a837168
Arslan Javaid <arslan.leo@live.com>
2016-10-05 13:29:51 +0500

    Adding step to courses.

---
+++ tutorial/migrations/0002_step.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tutorial', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Step',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255)),
+                ('description', models.TextField()),
+                ('content', models.TextField(default=b'', blank=True)),
+                ('order', models.IntegerField(default=0)),
+                ('course', models.ForeignKey(to='tutorial.Course')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
